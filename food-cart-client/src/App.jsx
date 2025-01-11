c0c60bb64113eee9be510fb0773d1e0ad6251d17
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-11 21:37:52 +0600

    component: add FoodItem

--- food-cart-client/src/App.jsx
+++ food-cart-client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Signin />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/home" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ food-cart-client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import FoodItem from "../components.jsx/FoodItem";
+
+export default function Home() {
+  return <FoodItem title="food title" desc="food description" />;
+}
