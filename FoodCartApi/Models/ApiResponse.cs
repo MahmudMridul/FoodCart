db41e2f50da86d05c43f9ef39643bd77990ae977
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-14 07:34:15 +0600

    add: User, RefreshToken, ApiResponse

---
+++ FoodCartApi/Models/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace FoodCartApi.Models
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; } = null;
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string>? Errors { get; set; } = null;
+
+        public static ApiResponse Create(HttpStatusCode statusCode, object? data = default, bool success = false, string msg = "", List<string>? errors = null)
+        {
+            return new ApiResponse()
+            {
+                Data = data,
+                Success = success,
+                Message = msg,
+                StatusCode = statusCode,
+                Errors = errors
+            };
+        }
+    }
+}
---
+++ FoodCartApi/Models/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodCartApi.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? RevokedOn { get; set; }
+
+        public string UserId { get; set; } = null!;
+        public User? User { get; set; }
+    }
+}
