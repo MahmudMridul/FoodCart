f744c1505ff9411cc0c7079a0ac123aad94b04a6
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-14 07:38:13 +0600

    add: Dtos - Signin, Signup

---
+++ FoodCartApi/Models/Dtos/SigninDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodCartApi.Models.Dtos
+{
+    public class SigninDto
+    {
+        public string NameOrEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
