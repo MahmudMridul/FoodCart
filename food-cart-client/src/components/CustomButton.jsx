ba7ad22bf96506476505dc6f27e3d83211423dfc
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-17 09:36:20 +0600

    update: add start icon, end icon, color props

--- food-cart-client/src/components/CustomButton.jsx
+++ food-cart-client/src/components/CustomButton.jsx
@@ -5,26 +5,35 @@ CustomButton.propTypes = {
 	label: PropType.string.isRequired,
 	size: PropType.string,
 	variant: PropType.string,
+	color: PropType.string,
 	onClick: PropType.func.isRequired,
 	disabled: PropType.bool,
 	sx: PropType.object,
+	startIcon: PropType.node,
+	endIcon: PropType.node,
 };
 
 export default function CustomButton({
 	label,
-	size,
-	variant,
+	size = "medium",
+	variant = "outlined",
+	color = "primary",
 	onClick,
 	disabled,
 	sx,
+	startIcon = null,
+	endIcon = null,
 }) {
 	return (
 		<Button
-			size={size ? size : "medium"}
-			variant={variant ? variant : "outlined"}
+			size={size}
+			variant={variant}
+			color={color}
 			onClick={onClick}
 			disabled={disabled}
 			sx={{ fontWeight: "bold", textTransform: "none", ...sx }}
+			startIcon={startIcon}
+			endIcon={endIcon}
 		>
 			{label}
 		</Button>
