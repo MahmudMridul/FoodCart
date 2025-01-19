697cff9d0649c7b62a1916dd292a9e87ac1ddb4f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-19 18:47:32 +0600

    add: GetAllFoodItems api

---
+++ FoodCartApi/Controllers/FoodItemController.cs
@@ -0,0 +1,67 @@
+﻿using FoodCartApi.Db;
+using FoodCartApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+
+
+namespace FoodCartApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public FoodItemController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<ApiResponse>> GetAllFoodItems()
+        {
+            ApiResponse res;
+            try 
+            {
+                List<FoodItem> foodItems = await _context.FoodItems.AsNoTracking().ToListAsync();
+                res = ApiResponse.Create(HttpStatusCode.OK, success: true, data: foodItems, msg: "Retrieved all food items");
+                return Ok(res);
+            } 
+            catch (Exception e) 
+            {
+                List<string> errs = new List<string> { e.Message, e.StackTrace };
+                res = ApiResponse.Create(HttpStatusCode.InternalServerError, msg: "An unexpected error occurred", errors: errs);
+                return StatusCode((int)HttpStatusCode.InternalServerError, res);
+            }
+        }
+
+        //// GET api/<FoodItemController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<FoodItemController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<FoodItemController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<FoodItemController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
--- FoodCartDb/DbQueries.sql
+++ FoodCartDb/DbQueries.sql
@@ -11,3 +11,10 @@ select u.FirstName, u.LastName, u.UserName, u.Email, r.Name as Role
 from Users u 
 inner join UserRoles ur on u.Id = ur.UserId
 inner join Roles r on ur.RoleId = r.Id;
+
+select * 
+from FoodItems;
+
+insert into FoodItems (Name, Description, Price, ImageUrl) values 
+('Biriyani', 'Chicken biriyani', 200, 'biriyani.jpg'),
+('Burger', 'Chicken burger', 200, 'burger.jpg');
