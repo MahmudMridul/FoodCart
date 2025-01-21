6c2d0da50697b5eaf6c90be968036da8ae5373b7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-21 22:03:09 +0600

    db: insert queries

--- FoodCartDb/DbQueries.sql
+++ FoodCartDb/DbQueries.sql
@@ -16,8 +16,13 @@ select *
 from FoodItems;
 
 insert into FoodItems (Name, Description, Price, ImageUrl) values 
-('Biriyani', 'Chicken biriyani', 200, 'biriyani.jpg'),
-('Burger', 'Chicken burger', 200, 'burger.jpg');
+('Bread & Egg', 'Soft bread slices toasted with fluffy scrambled eggs, lightly seasoned for a simple, hearty breakfast.', 150, 'bread-egg.jpg'),
+('Chicken Steak', 'Tender chicken fillet grilled to perfection, served with savory sauce and a side of fresh vegetables.', 500, 'chicken-steak.jpg'),
+('Fried Rice', 'Fluffy rice stir-fried with veggies, eggs, and choice of protein, seasoned with soy sauce and spices.', 300, 'fried-rice.jpg'),
+('Fries', 'Crispy golden fries, perfectly salted and served hot, ideal for snacking or as a side.', 100, 'fries.jpg'),
+('Lobster', 'Succulent lobster cooked to perfection, served with melted butter and fresh lemon wedges.', 500, 'lobster.jpg'),
+('Pancake', 'Fluffy pancakes stacked high, drizzled with syrup and topped with butter for a sweet breakfast treat.', 150, 'pancake.jpg'),
+('Pizza', 'Cheesy, hot pizza topped with your favorite ingredients on a perfectly baked crust.', 600, 'pizza.jpg');
 
 update FoodItems
 set Description = 'Juicy grilled chicken patty, fresh lettuce, tomato, and creamy mayo, all tucked in a soft sesame bun.' 
