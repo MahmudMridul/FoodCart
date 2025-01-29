77720da3a52681f6a14fbc7bdcb854ae3cebb9f9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-29 16:07:03 +0600

    refactor: add required attribute

--- FoodCartApi/Models/Dtos/SignupDto.cs
+++ FoodCartApi/Models/Dtos/SignupDto.cs
@@ -1,11 +1,18 @@
-﻿namespace FoodCartApi.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodCartApi.Models.Dtos
 {
     public class SignupDto
     {
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
